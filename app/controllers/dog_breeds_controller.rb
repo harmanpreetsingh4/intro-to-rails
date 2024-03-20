0ee580f9c5ab5bc4093738388a6583863adcb087
HarmanpreetSingh <harmanpreetsingh4@academic.rrc.ca>
2024-03-19 21:48:09 -0500

    Updated dog breed controller

--- app/controllers/dog_breeds_controller.rb
+++ app/controllers/dog_breeds_controller.rb
@@ -2,4 +2,8 @@ class DogBreedsController < ApplicationController
   def index
     @dog_breeds = DogBreed.where("breed_name LIKE ?", "%#{params[:search]}%")
   end
+  def show
+    @dog_breed = DogBreed.find(params[:id])
+    @sub_breeds = @dog_breed.sub_breeds
+  end
 end
